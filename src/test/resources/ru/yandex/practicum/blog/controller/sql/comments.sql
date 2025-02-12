f2d98f68926c70121845304769550b74ef840f2d
Митянин Федор Юрьевич <f.mityanin@s7.ru>
2025-02-13 00:06:21 +0700

    add test

---
+++ src/test/resources/ru/yandex/practicum/blog/controller/sql/comments.sql
@@ -0,0 +1,4 @@
+DELETE FROM comments;
+INSERT INTO comments (id, post_id, content, created_at, updated_at) VALUES (1, 2, 'aasasd', '2025-01-25 15:21:10.810867', '2025-01-25 15:21:10.810867');
+INSERT INTO comments (id, post_id, content, created_at, updated_at) VALUES (2, 2, 'vsvfvdvdfvdf', '2025-01-25 15:21:10.810867', '2025-01-25 15:21:10.810867');
+INSERT INTO comments (id, post_id, content, created_at, updated_at) VALUES (3, 3, 'wqwqwdqwd', '2025-01-25 15:21:10.810867', '2025-01-25 15:21:10.810867');
---
+++ src/test/resources/ru/yandex/practicum/blog/controller/sql/likes.sql
@@ -0,0 +1,7 @@
+DELETE FROM likes;
+INSERT INTO likes (id, post_id, user_id, created_at) VALUES (2, 4, 1, '2025-01-25 15:22:54.756703');
+INSERT INTO likes (id, post_id, user_id, created_at) VALUES (3, 4, 2, '2025-01-25 15:22:54.756703');
+INSERT INTO likes (id, post_id, user_id, created_at) VALUES (4, 4, 3, '2025-01-25 15:22:54.756703');
+INSERT INTO likes (id, post_id, user_id, created_at) VALUES (5, 3, 4, '2025-01-25 15:22:54.756703');
+INSERT INTO likes (id, post_id, user_id, created_at) VALUES (6, 3, 5, '2025-01-25 15:22:54.756703');
+
